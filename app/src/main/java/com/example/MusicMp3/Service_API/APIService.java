631bc3b419dbfdb049fd0b0cd1b6466923e2dead
hungt29 <73229448+hungt29@users.noreply.github.com>
2022-12-14 23:35:11 +0700

    update by hungt29

--- app/src/main/java/com/example/MusicMp3/Service_API/APIService.java
+++ app/src/main/java/com/example/MusicMp3/Service_API/APIService.java
@@ -1,7 +1,7 @@
 package com.example.MusicMp3.Service_API;
 
 public class APIService {
-    private static String base_url = "https://music4b.000webhostapp.com/Server/";
+    private static String base_url = "https://tonghung19020552.000webhostapp.com/Server/";
     public static Dataservice getService(){
         return APIRetrofitClient.getClient(base_url).create(Dataservice.class);
     }
